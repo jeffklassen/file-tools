393c84526ce1284fa8d5dcc92a13b3b9fc646fef
Jeffrey Klassen <jeff.klassen@siginttech.com>
2016-10-08 10:59:58 +0200

    install more prereques, promisify image processing

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node
 
 EXPOSE 3000
 
-RUN apt update && apt install tesseract-ocr -y
+RUN apt update && apt install tesseract-ocr imagemagick ghostscript poppler-utils  -y
 WORKDIR /app
 ADD . /app
 RUN npm install
--- routes/api.js
+++ routes/api.js
@@ -1,32 +1,60 @@
 var express = require('express');
 var router = express.Router();
 var colors = require('colors');
-
+var tesseract = require('node-tesseract');
+var PDFImage = require("pdf-image").PDFImage;
+var Promise = require('promise');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     console.log('Index page requested, not doing anything.'.yellow);
     res.send("welcome to file land.")
 });
 
-router.post('/ocr', function(req, res, next) {
+router.post('/ocr', function (req, res, next) {
     console.log('File uploading'.yellow);
     var sampleFile;
- 
+
     if (!req.files) {
         res.send('No files were uploaded.');
         return;
     }
- 
+
     sampleFile = req.files.sampleFile;
-    sampleFile.mv('/tmp/filename.pdf', function(err) {
+    /*
+    sampleFile.mv('/tmp/filename.pdf', function (err) {
         if (err) {
             res.status(500).send(err);
         }
         else {
-            res.send('File uploaded!');
+            tesseract.process('/tmp/filename.pdf', function (err, text) {
+                if (err) {
+                    console.error(err);
+                    res.status(500).send(err);
+                } else {
+                    console.log(text);
+                    res.send(text);
+                }
+            });
+
         }
-    });
+    });*/
+
+    var filemv = Promise.denodeify(sampleFile.mv);
+    var tmpfilename = '/tmp/filename.pdf';
+    tesseract.process = Promise.denodeify(tesseract.process)
+    filemv(tmpfilename)
+        .then(function () {
+            return tesseract.process(tmpfilename);
+        })
+        .then(function(text){
+             console.log(text);
+                    res.send(text);
+        }).catch(function(err)
+        {
+             console.error(err);
+                    res.status(500).send(err);
+        });
 });
 
 
