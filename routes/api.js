89f7bee583dbec4fbf84ad80dba22dc755a19325
Jeffrey Klassen <jeff.klassen@siginttech.com>
2016-11-12 16:34:42 +0100

    fixed multi upload problem

--- routes/api.js
+++ routes/api.js
@@ -5,17 +5,19 @@ var tesseract = require('node-tesseract');
 var PDFImage = require('pdf-image').PDFImage;
 var Promise = require('promise');
 var _ = require('underscore');
-var uuid = require('uuid')
+var uuid = require('uuid');
 var glob = Promise.denodeify(require('glob'));
 var fs = require('fs');
 
+tesseract.process = Promise.denodeify(tesseract.process);
+
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     console.log('Index page requested, not doing anything.'.yellow);
     res.send('welcome to file land.');
 });
 
-router.post('/ocr', function (req, res, next) {
+router.post('/ocr', function (req, res) {
     console.log('File uploading'.yellow);
     var sampleFile;
 
@@ -24,20 +26,24 @@ router.post('/ocr', function (req, res, next) {
         return;
     }
 
-    sampleFile = req.files.sampleFile;
+    sampleFile = req.files.ocrFile;
 
     sampleFile.mv = Promise.denodeify(sampleFile.mv);
     var docUUID = uuid.v4();
     var tmpfilename = '/tmp/' + docUUID + '.pdf';
-    tesseract.process = Promise.denodeify(tesseract.process);
+
+
+    console.log(tesseract.process);
 
     sampleFile.mv(tmpfilename)
         .then(() => {
             var options = {};
             options.convertOptions = {};
-            options.convertOptions['-quality'] = "100";
-            options.convertOptions['-density'] = "150";
+            options.convertOptions['-quality'] = '100';
+            options.convertOptions['-density'] = '150';
+
             var pdfImage = new PDFImage(tmpfilename, options);
+
             return Promise.all([pdfImage, pdfImage.numberOfPages()]);
 
         })
@@ -45,15 +51,15 @@ router.post('/ocr', function (req, res, next) {
             var pdfImage = data[0];
             var numberOfPages = data[1];
 
-            //convert all pdfPages to images. Set the pdfImage as the context.
+            // convert all pdfPages to images. Set the pdfImage as the context.
             return Promise.all(
                 _.map(
-                    _.range(numberOfPages), pageNum =>(
+                    _.range(numberOfPages), pageNum => (
                         pdfImage.convertPage(pageNum)
-                        .then(data => {
-                             console.log('dataaaaaa',data);
-                            return data;
-                        })
+                            .then(data => {
+                                console.log('dataaaaaa', data);
+                                return data;
+                            })
                     ), pdfImage));
 
         })
@@ -61,13 +67,14 @@ router.post('/ocr', function (req, res, next) {
             console.log('DATA', filenames);
 
             // process all converted files
-            return Promise.all(_.map(filenames, filename => {
+            return Promise.all(filenames.map(filename => {
+                console.log('calling tess', filename);
                 return tesseract.process(filename)
                     .then(text => {
                         return text.trim();
-                    }).
-                    catch(err =>{
-                        console.log('ERRRROR', err)
+                    })
+                    .catch(err => {
+                        console.log('ERRRROR', err);
                     });
             }));
         })
@@ -85,16 +92,16 @@ router.post('/ocr', function (req, res, next) {
             glob('/tmp/' + docUUID + '*')
                 .then(files => {
                     console.log('DELETEING FILES', files);
-                    return Promise.all(_.map(files, file =>{
+                    return Promise.all(_.map(files, file => {
                         return unlink(file);
                     }));
                 })
                 .then(files => {
-                    console.log('DELETED', files)
+                    console.log('DELETED', files);
                 })
-                .catch(err => (
-                    console.log('ERROR REMOVING FILES', err, files)
-                ))
+                .catch((err, files) => {
+                    console.log('ERROR REMOVING FILES', err, files);
+                });
         });
 });
 
