0763739f860b2eda7ecf28687e4cd684e9d09879
Jeffrey Klassen <jklassen@novetta.com>
2017-01-01 10:44:46 +0100

    sample

--- client/getOcr.js
+++ client/getOcr.js
@@ -2,17 +2,12 @@ const fs = require('fs');
 const Promise = require('promise');
 const post = Promise.denodeify(require('request').post);
 
-
 module.exports = (path) => {
     const formData = {
-
         ocrFile: fs.createReadStream(path)
-
     };
     console.log('posting form data');
     return post({ url: 'http://localhost:3000/ocr', formData })
-        .then(res => {
-            return res.body;
-        });
-       
+        .then(res => res.body);
+
 };
--- client/index.js
+++ client/index.js
@@ -1,14 +1,26 @@
-var getOcr = require('./getOcr');
+const getOcr = require('./getOcr');
+const Knwl = require('knwl.js');
+let knwlInstance = new Knwl('english');
+//knwlInstance.register('dates', require('./default_plugins/dates'));
 
-getOcr('/Users/jeff/Downloads/Done_Washer_Dryer and Misc items.pdf')
+
+//getOcr('/Users/jeff/Downloads/Done_Washer_Dryer and Misc items.pdf')
+getOcr('/Users/jeff/Downloads/Done_Scanned_20161210-0103.pdf')
     .then(text => {
+        //replace all '.' with ',' because the OCR often thinks commas are periods and knwl uses '.' to terminate dates
+        text = text.replace(/\./g, ',');
+        knwlInstance.init(text);
         console.log(text);
-        const re = /\d\d\/\d\d\/\d\d\d\d/gi;
-        let found = text.match(re);
-        console.log(found);
+        console.log('dates', knwlInstance.get('dates'));
+        let dates = knwlInstance.get('dates');
+        dates = dates.filter(date => {
+            let thisYear = new Date().getFullYear();
+            return date.year > thisYear - 10 && date.year < thisYear + 10;
+        });
+        console.log('filtered dates', dates);
+        //console.log('places', knwlInstance.get('places'));
     })
     .catch(err => {
-        console.log('ERROR GETTING OCR FROM SERVER', err)
-    })
-    ;
+        console.log('ERROR GETTING OCR FROM SERVER', err);
+    });
 
